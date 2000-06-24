2568cd54f260b390c4ab7bce4c24b797700795ae
tufte <tufte>
2000-06-24 16:10:08 +0000

    New runtime exception PEException (I use it like an assert to catch cases that shouldn't happen - I'm open to better suggestions).
    Class to compare Attribute names.

---
+++ src/niagara/utils/AttrNameComparator.java
@@ -0,0 +1,44 @@
+package niagara.utils;
+
+/*
+ * $RCSfile:
+ * $Revision:
+ * $Date:
+ * $Author:
+ */
+
+
+/**
+ * A <code> Comparator </code> which compares the names of two
+ * attributes based on lexigraphical ordering.
+ * Used for sorting arrays of attributes based on attribute
+ * name.
+ *
+ * @version 1.0
+ *
+ * @author Kristin Tufte
+ */
+
+import java.util.*;
+import java.io.*;
+import org.w3c.dom.*;
+
+public class AttrNameComparator implements Comparator, Serializable {
+    public AttrNameComparator() {}
+
+    /**
+     * Compares two DOM Attr objects based on their name.
+     * Names are compared using the standard String.compare
+     * function. Arguments must implement the DOM Attr interface
+     *
+     * @param o1 First Attr object to be compared.
+     * @param o2 Second Attr object to be compared.
+     *
+     * @return Returns a negative int, zero, or positive int if
+     * o1 is less than, equal to, or greater than o2, respectively.
+     */
+    public int compare(Object o1, Object o2) {
+	return ((Attr)o1).getName().compareTo(((Attr)o2).getName());	
+    }
+}
+
---
+++ src/niagara/utils/PEException.java
@@ -0,0 +1,43 @@
+package niagara.utils;
+
+/**
+ * ProgrammingErrorException.java
+ * Created: March 30, 2000 
+ *
+ * @author Kristin Tufte 
+ * @version
+ */
+
+/**
+ * <code> PEException </code>  A Programming Error Exception -
+ * to be thrown when there is a programming error
+ * detected.  This is similar to an assert.  To be used
+ * when the code detects a situation it thinks should never
+ * happen.  Ideal action is to pop this exception up to the
+ * top of the operator/query thread and shut down the query
+ * with an error message - system doesn't need to crash.
+ * @see Exception
+ */
+
+public class PEException extends RuntimeException {
+  
+    /**
+     * constructor
+     * @see Exception
+     */
+    public PEException() {  
+        super("Programming Error Exception:");
+    }
+    
+    /**
+     * constructor
+     * @param msg the exception message
+     * @see Exception
+     */
+    public PEException(String msg) 
+    {
+        super("Programming Error Exception:"+msg);
+        
+    }
+    
+} // PEException
