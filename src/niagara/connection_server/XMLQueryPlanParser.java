646fd90a1aa14218ed98fe18fd6768b76accbf51
vpapad <vpapad>
2000-07-10 21:54:33 +0000

    Scan in XML query plans does not need the extra index attribute any more.

--- src/niagara/connection_server/XMLQueryPlanParser.java
+++ src/niagara/connection_server/XMLQueryPlanParser.java
@@ -91,236 +91,270 @@ public class XMLQueryPlanParser {
 	    parseOperator(el);
 	}
 
-
 	// Now that all inputs are handled 
 	// create a logNode for this operator
 	if (nodeName.equals("scan")) {
-
-	    scanOp op = (scanOp) operators.Scan.clone();
-	    op.setSelectedAlgoIndex(0);
-
-	    String indexAttr = e.getAttribute("index");
-	    String inputAttr = e.getAttribute("input");
-	    String typeAttr = e.getAttribute("type");
-	    String regexpAttr = e.getAttribute("regexp");
-
-	    int type;
-	    if (typeAttr.equals("tag")) {
-		type = varType.TAG_VAR;
-	    }
-	    else if (typeAttr.equals("element")) {
-		type = varType.ELEMENT_VAR;
-	    }
-	    else { // (typeAttr.equals("content"))
-		type = varType.CONTENT_VAR;
-	    }
-	    
-	    schemaAttribute resultSA = 
-		new schemaAttribute(Integer.parseInt(indexAttr) + 1, type);
-
-	    logNode input = (logNode) ids2nodes.get(inputAttr);
-
-	    // Copy the varTbl and schema from the node downstream, or create 
-	    // empty ones if they don't exist
-
-	    varTbl qpVarTbl; Schema sc;
-
-	    if (input.getVarTbl() != null) {
-		qpVarTbl = new varTbl(input.getVarTbl());
-		sc = new Schema(input.getSchema()); 
-	    }
-	    else {
-		qpVarTbl = new varTbl();
-		sc = new Schema();
-		sc.addSchemaUnit(new SchemaUnit(null, -1));
-	    }
-
-
-	    Scanner scanner;
-	    regExp redn = null;
-	    try {
-		scanner = new Scanner(new StringReader(regexpAttr));
-		REParser rep = new REParser(scanner);
-		redn = (regExp) rep.parse().value;
-		rep.done_parsing();
-	    }
-	    catch (Exception ex) {
-		System.err.println("Error while parsing: " + regexpAttr);
-		ex.printStackTrace();
-		throw new InvalidPlanException();
-	    }
-
-	    // Register variable -> resultSA
-	    qpVarTbl.addVar("$" + id, resultSA);
-
-	    schemaAttribute sa = 
-		new schemaAttribute(Integer.parseInt(indexAttr));
-
-	    sc.addSchemaUnit(new SchemaUnit(redn, sc.numAttr()));
-	    op.setScan(sa, redn);
-
-	    logNode scanNode = new logNode(op, input);
-
-	    ids2nodes.put(id, scanNode);
-
-	    scanNode.setSchema(sc);
-	    scanNode.setVarTbl(qpVarTbl);
+	    handleScan(e);
 	}
-
 	else if (nodeName.equals("select")) {
-	    selectOp op = (selectOp) operators.Select.clone();
-	    op.setSelectedAlgoIndex(0);
-
-	    String inputAttr = e.getAttribute("input");
-	    NodeList children = e.getChildNodes();
-
-	    Element predElt = null;
-
-	    for (int i=0; i < children.getLength(); i++) {
-		if (children.item(i) instanceof Element) {
-		    predElt = (Element) children.item(i);
-		    break;
-		}
-	    }
-
-	    logNode input =  (logNode) ids2nodes.get(inputAttr);
-
-	    varTbl qpVarTbl = input.getVarTbl();
-	    predicate pred = parsePreds(predElt, qpVarTbl, null);
-
-	    Vector v = new Vector(); v.addElement(pred);
-	    op.setSelect(v);
-
-	    logNode selectNode = new logNode(op, input);
-	    ids2nodes.put(id, selectNode);
-	    
-	    // Nothing changes for the variables
-	    // Just use whatever variable table the node downstream has
-	    selectNode.setSchema(input.getSchema());
-	    selectNode.setVarTbl(qpVarTbl);
+	    handleSelect(e);
 	}
 	else if (nodeName.equals("join")) {
-	    joinOp op = (joinOp) operators.Join.clone();
-	    String className = e.getAttributeNode("physical").getValue();
-	    try {
-		op.setSelectedAlgorithm(className);
-	    }
-	    catch (Exception ex) {
-		System.err.println("Invalid algorithm: " + className);
-		throw new InvalidPlanException();
-	    }
-
-	    logNode left = (logNode) ids2nodes.get(inputs.elementAt(0));
-	    logNode right = (logNode) ids2nodes.get(inputs.elementAt(1));
-
-	    NodeList children = e.getChildNodes();
-	    Element predElt = null;
-
-	    for (int i=0; i < children.getLength(); i++) {
-		if (children.item(i) instanceof Element) {
-		    predElt = (Element) children.item(i);
-		    break;
-		}
-	    }
-
-	    varTbl leftv = left.getVarTbl();
-	    varTbl rightv = right.getVarTbl();
-
-	    predicate pred = parsePreds(predElt, leftv, rightv);
-	    op.setJoin(pred);
-
-	    logNode joinNode = new logNode(op, left, right);
-	    ids2nodes.put(id, joinNode);
-	    
-	    //Merge the two schemas 
-	    Schema sc = Util.mergeSchemas(left.getSchema(), right.getSchema());
-	    joinNode.setSchema(sc);
-
-	    //Merge the two varTbls 
-	    joinNode.setVarTbl(Util.mergeVarTbl(leftv, rightv, left.getSchema().numAttr()));
+	    handleJoin(e, inputs);
 	}
-
 	else if (nodeName.equals("dtdscan")) {
-	    // The node's children contain URLs
-	    Vector urls = new Vector();
-	    NodeList children = ((Element) e).getChildNodes();
-	    for (int i=0; i < children.getLength(); i++) {
-		Node child = children.item(i);
-		if (child.getNodeType() != Node.ELEMENT_NODE)
-		    continue;
-		urls.addElement(((Element) child).getAttribute("value"));
-	    }
-
-	    dtdScanOp op = (dtdScanOp) operators.DtdScan.clone();
-	    op.setDocs(urls);
-	    logNode node = new logNode(op);
-	    ids2nodes.put(id, node);
+	    handleDtdScan(e);
 	}
-
 	else if (nodeName.equals("construct")) {
-	    constructOp op = (constructOp) operators.Construct.clone();
-	    op.setSelectedAlgoIndex(0);
-	    NodeList children = e.getChildNodes();
-	    String content = "";
-	    for (int i=0; i < children.getLength(); i++) {
-		if (children.item(i).getNodeType() != Node.ELEMENT_NODE)
-		    continue;
-		content = content + flatten(children.item(i));
-	    }
-	    String inputAttr = e.getAttribute("input");
-
-	    logNode input = (logNode) ids2nodes.get(inputAttr);
-	    Scanner scanner;
-	    constructBaseNode cn = null;
-
-	    try {
-		scanner = new Scanner(new StringReader(content));
-		ConstructParser cep = new ConstructParser(scanner);
-		cn = (constructBaseNode) cep.parse().value;
-		cep.done_parsing();
-	    }
-	    catch (Exception ex) {
-		System.err.println("Error while parsing: "+ content);
-		throw new InvalidPlanException();
-	    }
-
-	    varTbl qpVarTbl = input.getVarTbl();
-	    System.out.println("Variable table in construct: ");
-	    qpVarTbl.dump();
-
-	    cn.replaceVar(qpVarTbl);
-	    op.setConstruct(cn);
-
-	    logNode node = new logNode(op, (logNode) ids2nodes.get(inputAttr));
-
-	    ids2nodes.put(id, node);
-	    node.setVarTbl(qpVarTbl);
+	    handleConstruct(e);
 	}
 	else if (nodeName.equals("firehosescan")) {
-	    System.out.println("firehosescan");
-
-	    String host = e.getAttribute("host");
-	    int port = Integer.parseInt(e.getAttribute("port"));
-	    int rate = Integer.parseInt(e.getAttribute("rate"));
-	    int iters = Integer.parseInt(e.getAttribute("iters"));
-	    String typeStr = e.getAttribute("type");
-	    int type;
-	    // XXX FIXME we should import FirehoseClient to get to these values
-	    if (typeStr.equals("file"))
-		type = 1;
-	    else // gen
-		type = 2;
-	    String desc = e.getAttribute("desc");
-	    FirehoseSpec fhspec = 
-		new FirehoseSpec(port, host, rate, type, desc, iters);
-	    
-	    FirehoseScanOp op = 
-		(FirehoseScanOp) operators.FirehoseScan.clone();
-	    
-	    op.setFirehoseScan(fhspec);
-	    logNode node = new logNode(op);
-	    ids2nodes.put(id, node);	    
+	    handleFirehoseScan(e);
 	}
+    }
+
+    void handleScan(Element e) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	scanOp op = (scanOp) operators.Scan.clone();
+	op.setSelectedAlgoIndex(0);
+
+	String id = e.getAttribute("id");
+
+	String inputAttr = e.getAttribute("input");
+	String typeAttr = e.getAttribute("type");
+	String regexpAttr = e.getAttribute("regexp");
+	
+	int type;
+	if (typeAttr.equals("tag")) {
+	    type = varType.TAG_VAR;
+	}
+	else if (typeAttr.equals("element")) {
+	    type = varType.ELEMENT_VAR;
+	}
+	else { // (typeAttr.equals("content"))
+	    type = varType.CONTENT_VAR;
+	}
+	
+	logNode input = (logNode) ids2nodes.get(inputAttr);
+	
+	// Copy the varTbl and schema from the node downstream, or create 
+	// empty ones if they don't exist
+	
+	varTbl qpVarTbl; Schema sc;
+	
+	if (input.getVarTbl() != null) {
+	    qpVarTbl = new varTbl(input.getVarTbl());
+	    sc = new Schema(input.getSchema()); 
+	}
+	else {
+	    qpVarTbl = new varTbl();
+	    sc = new Schema();
+	    sc.addSchemaUnit(new SchemaUnit(null, -1));
+	}
+	
+	
+	Scanner scanner;
+	regExp redn = null;
+	try {
+	    scanner = new Scanner(new StringReader(regexpAttr));
+	    REParser rep = new REParser(scanner);
+	    redn = (regExp) rep.parse().value;
+	    rep.done_parsing();
+	}
+	catch (Exception ex) {
+	    System.err.println("Error while parsing: " + regexpAttr);
+	    ex.printStackTrace();
+	    throw new InvalidPlanException();
+	}
+	
+	schemaAttribute resultSA = 
+	    new schemaAttribute(sc.numAttr(), type);
+	
+	// Register variable -> resultSA
+	qpVarTbl.addVar("$" + id, resultSA);
+	
+	schemaAttribute sa = 
+	    new schemaAttribute(sc.numAttr()-1);
+	
+	sc.addSchemaUnit(new SchemaUnit(redn, sc.numAttr()));
+	op.setScan(sa, redn);
+	
+	logNode scanNode = new logNode(op, input);
+	
+	ids2nodes.put(id, scanNode);
+	
+	scanNode.setSchema(sc);
+	scanNode.setVarTbl(qpVarTbl);
+    }
+
+    void handleSelect(Element e) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	String id = e.getAttribute("id");
+
+	selectOp op = (selectOp) operators.Select.clone();
+	op.setSelectedAlgoIndex(0);
+	
+	String inputAttr = e.getAttribute("input");
+	NodeList children = e.getChildNodes();
+	
+	Element predElt = null;
+	
+	for (int i=0; i < children.getLength(); i++) {
+	    if (children.item(i) instanceof Element) {
+		predElt = (Element) children.item(i);
+		break;
+	    }
+	}
+	
+	logNode input =  (logNode) ids2nodes.get(inputAttr);
+	
+	varTbl qpVarTbl = input.getVarTbl();
+	predicate pred = parsePreds(predElt, qpVarTbl, null);
+	
+	Vector v = new Vector(); v.addElement(pred);
+	op.setSelect(v);
+	
+	logNode selectNode = new logNode(op, input);
+	ids2nodes.put(id, selectNode);
+	
+	// Nothing changes for the variables
+	// Just use whatever variable table the node downstream has
+	selectNode.setSchema(input.getSchema());
+	selectNode.setVarTbl(qpVarTbl);
+    }
+
+    void handleJoin(Element e, Vector inputs) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	String id = e.getAttribute("id");
+
+	joinOp op = (joinOp) operators.Join.clone();
+	String className = e.getAttributeNode("physical").getValue();
+	try {
+	    op.setSelectedAlgorithm(className);
+	}
+	catch (Exception ex) {
+	    System.err.println("Invalid algorithm: " + className);
+	    throw new InvalidPlanException();
+	}
+	
+	logNode left = (logNode) ids2nodes.get(inputs.elementAt(0));
+	logNode right = (logNode) ids2nodes.get(inputs.elementAt(1));
+	
+	NodeList children = e.getChildNodes();
+	Element predElt = null;
+	
+	for (int i=0; i < children.getLength(); i++) {
+	    if (children.item(i) instanceof Element) {
+		predElt = (Element) children.item(i);
+		break;
+	    }
+	}
+	
+	varTbl leftv = left.getVarTbl();
+	varTbl rightv = right.getVarTbl();
+	
+	predicate pred = parsePreds(predElt, leftv, rightv);
+	op.setJoin(pred);
+	
+	logNode joinNode = new logNode(op, left, right);
+	ids2nodes.put(id, joinNode);
+	
+	//Merge the two schemas 
+	Schema sc = Util.mergeSchemas(left.getSchema(), right.getSchema());
+	joinNode.setSchema(sc);
+	
+	//Merge the two varTbls 
+	joinNode.setVarTbl(Util.mergeVarTbl(leftv, rightv, left.getSchema().numAttr()));
+    }
+
+    void handleDtdScan(Element e) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	String id = e.getAttribute("id");
+
+	// The node's children contain URLs
+	Vector urls = new Vector();
+	NodeList children = ((Element) e).getChildNodes();
+	for (int i=0; i < children.getLength(); i++) {
+	    Node child = children.item(i);
+	    if (child.getNodeType() != Node.ELEMENT_NODE)
+		continue;
+	    urls.addElement(((Element) child).getAttribute("value"));
+	}
+	
+	dtdScanOp op = (dtdScanOp) operators.DtdScan.clone();
+	op.setDocs(urls);
+	logNode node = new logNode(op);
+	ids2nodes.put(id, node);
+    }
+
+    void handleFirehoseScan(Element e) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	String id = e.getAttribute("id");
+
+	String host = e.getAttribute("host");
+	int port = Integer.parseInt(e.getAttribute("port"));
+	int rate = Integer.parseInt(e.getAttribute("rate"));
+	int iters = Integer.parseInt(e.getAttribute("iters"));
+	String typeStr = e.getAttribute("type");
+	int type;
+	// XXX FIXME we should import FirehoseClient to get to these values
+	if (typeStr.equals("file"))
+	    type = 1;
+	else // gen
+	    type = 2;
+	String desc = e.getAttribute("desc");
+	FirehoseSpec fhspec = 
+	    new FirehoseSpec(port, host, rate, type, desc, iters);
+	
+	FirehoseScanOp op = 
+	    (FirehoseScanOp) operators.FirehoseScan.clone();
+	
+	op.setFirehoseScan(fhspec);
+	logNode node = new logNode(op);
+	ids2nodes.put(id, node);	    
+    }
+
+    void handleConstruct(Element e) 
+	throws CloneNotSupportedException, InvalidPlanException {
+	String id = e.getAttribute("id");
+
+	constructOp op = (constructOp) operators.Construct.clone();
+	op.setSelectedAlgoIndex(0);
+	NodeList children = e.getChildNodes();
+	String content = "";
+	for (int i=0; i < children.getLength(); i++) {
+	    if (children.item(i).getNodeType() != Node.ELEMENT_NODE)
+		continue;
+	    content = content + flatten(children.item(i));
+	}
+	String inputAttr = e.getAttribute("input");
+	
+	logNode input = (logNode) ids2nodes.get(inputAttr);
+	Scanner scanner;
+	constructBaseNode cn = null;
+	
+	try {
+	    scanner = new Scanner(new StringReader(content));
+	    ConstructParser cep = new ConstructParser(scanner);
+	    cn = (constructBaseNode) cep.parse().value;
+	    cep.done_parsing();
+	}
+	catch (Exception ex) {
+	    System.err.println("Error while parsing: "+ content);
+	    throw new InvalidPlanException();
+	}
+	
+	varTbl qpVarTbl = input.getVarTbl();
+	System.out.println("Variable table in construct: ");
+	qpVarTbl.dump();
+	
+	cn.replaceVar(qpVarTbl);
+	op.setConstruct(cn);
+	
+	logNode node = new logNode(op, (logNode) ids2nodes.get(inputAttr));
+	
+	ids2nodes.put(id, node);
+	node.setVarTbl(qpVarTbl);
     }
 
     predicate parsePreds(Element e, varTbl leftv, varTbl rightv) {
@@ -341,21 +375,18 @@ public class XMLQueryPlanParser {
 	    predicate left = parsePreds((Element) e.getChildNodes().item(0), leftv, rightv);
 	    predicate right = parsePreds((Element) e.getChildNodes().item(1), leftv, rightv);
 
-	    p = new predLogOpNode(opType.AND, left, right);
-	    return p;
+	    return new predLogOpNode(opType.AND, left, right);
 	}
 	else if (e.getNodeName().equals("or")) {
 	    predicate left = parsePreds((Element) e.getChildNodes().item(0), leftv, rightv);
 	    predicate right = parsePreds((Element) e.getChildNodes().item(1), leftv, rightv);
 
-	    p = new predLogOpNode(opType.OR, left, right);
-	    return p;
+	    return new predLogOpNode(opType.OR, left, right);
 	}
 	else if (e.getNodeName().equals("not")) {
 	    predicate child = parsePreds((Element) e.getChildNodes().item(0), leftv, rightv);
 
-	    p = new predLogOpNode(opType.NOT, child);
-	    return p;
+	    return new predLogOpNode(opType.NOT, child);
 	}
 	else { // Relational operator
 	    data left = parseAtom((Element) e.getChildNodes().item(0), leftv, rightv);
@@ -377,8 +408,7 @@ public class XMLQueryPlanParser {
 	    else // eq
 		type = opType.EQ;
 
-	    p = new predArithOpNode(type, left, right);
-	    return p;
+	    return new predArithOpNode(type, left, right);
 	}
     }
 
