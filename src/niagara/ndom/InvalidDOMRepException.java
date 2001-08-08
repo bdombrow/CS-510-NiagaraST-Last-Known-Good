fce0230b319c4ff3e14b00970752883db5711c53
tufte <tufte>
2001-08-08 21:26:36 +0000

    New exception for when someone is trying to use TX stuff with xerces parser.

---
+++ src/niagara/ndom/InvalidDOMRepException.java
@@ -0,0 +1,39 @@
+package niagara.ndom;
+
+/**
+ * InvalidDOMRepException.java
+ * Created: August 2, 2001 
+ *
+ * @author Kristin Tufte 
+ * @version
+ */
+
+/**
+ * <code> InvalidDOMRepException </code> Some code requires
+ * a certain DOM represention, this error can be thrown
+ * when the wrong representation is detected
+ *
+ * @see Exception
+ */
+
+public class InvalidDOMRepException extends Exception {
+  
+    /**
+     * constructor
+     * @see Exception
+     */
+    public InvalidDOMRepException() {  
+        super("Invalid DOM Representation used");
+    }
+    
+    /**
+     * constructor
+     * @param msg the exception message
+     * @see Exception
+     */
+    public InvalidDOMRepException(String msg) 
+    {
+        super(msg);
+    }
+    
+} 
