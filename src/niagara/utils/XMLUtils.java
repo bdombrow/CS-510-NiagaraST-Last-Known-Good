70a89d0a43ac3ed78d57bd07916086a6d97dc6e7
vpapad <vpapad>
2002-04-17 03:10:12 +0000

    explosiveFlatten() !

--- src/niagara/utils/XMLUtils.java
+++ src/niagara/utils/XMLUtils.java
@@ -17,47 +17,65 @@ public class XMLUtils {
 
     public static String flatten(Node n) {
         StringBuffer sb = new StringBuffer();
-        flatten(n, sb);
+        flatten(n, sb, false);
         return sb.toString();
     }
 
-    public static void flatten(Node n, StringBuffer sb) {
+    public static String explosiveFlatten(Node n) {
+        StringBuffer sb = new StringBuffer();
+        flatten(n, sb, true);
+        return sb.toString();
+    }
+
+    public static void flatten(Node n, StringBuffer sb, boolean explode) {
 	short type = n.getNodeType();
 	if (type == Node.ELEMENT_NODE) {
 	    sb.append("<" + n.getNodeName());
 	    NamedNodeMap attrs = n.getAttributes();	    
 	    for (int i=0; i < attrs.getLength(); i++) {
 		Attr a = (Attr) attrs.item(i);
-		sb.append(" " + a.getName() + "=\"" + a.getValue() + "\"");
+		if (explode) {
+		    String namespaceURI = a.getNamespaceURI();
+		    if (namespaceURI != null && 
+			namespaceURI.equals("http://www.cse.ogi.edu/dot/niagara/")
+			&& a.getLocalName().equals("explode")) {
+			// Replace the attribute with its content
+			sb.append(" " + a.getValue());
+		    }
+		    else sb.append(" " + a.getName() + "=\"" + a.getValue() + "\"");
+		}
+		else 
+		    sb.append(" " + a.getName() + "=\"" + a.getValue() + "\"");
 	    }
 	    sb.append(">");
 	    NodeList nl = n.getChildNodes();
 	    for (int i=0; i < nl.getLength(); i++) {
-                flatten(nl.item(i), sb);
+                flatten(nl.item(i), sb, explode);
 	    }
 	    sb.append("</" + n.getNodeName() + ">");
-	} else if (type == Node.TEXT_NODE || type == Node.CDATA_SECTION_NODE) {
+	}
+	else if (type == Node.TEXT_NODE || type == Node.CDATA_SECTION_NODE) {
 	    sb.append(n.getNodeValue());
-	} else if (type == Node.DOCUMENT_NODE) {
-	    Node kid = n.getFirstChild();
-	    boolean done = false;
-            while (kid != null && done == false) {
-		short kidType = kid.getNodeType();
-		if(kidType == Node.TEXT_NODE ||
-		   kidType == Node.CDATA_SECTION_NODE ||
-		   kidType == Node.ELEMENT_NODE) {
-		    flatten(kid, sb);
-		    done = true;
-		} else {
-		    kid = kid.getNextSibling();
-		}
-	    }
-	    if(done == false) {
-		sb.append("<!-- Empty document node -->");
-	    }
-	} else {
+ 	} else if (type == Node.DOCUMENT_NODE) {
+ 	    Node kid = n.getFirstChild();
+ 	    boolean done = false;
+             while (kid != null && done == false) {
+ 		short kidType = kid.getNodeType();
+ 		if(kidType == Node.TEXT_NODE ||
+ 		   kidType == Node.CDATA_SECTION_NODE ||
+ 		   kidType == Node.ELEMENT_NODE) {
+ 		    flatten(kid, sb, explode);
+ 		    done = true;
+ 		} else {
+ 		    kid = kid.getNextSibling();
+ 		}
+ 	    }
+ 	    if(done == false) {
+ 		sb.append("<!-- Empty document node -->");
+ 	    }
+ 	} else {
 	    sb.append("<!-- XMLUtils.flatten() could not serialize this node -->");
-	    throw new RuntimeException("flatten" + type + " " + Node.DOCUMENT_NODE);
+ 	    throw new RuntimeException("flatten" + type + " " + Node.DOCUMENT_NODE);
 	}
     }
 }
