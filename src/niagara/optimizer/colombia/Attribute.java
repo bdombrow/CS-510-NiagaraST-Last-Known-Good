debc5833ba642a9f9eeb2471c2b5a800ab4168d3
vpapad <vpapad>
2002-10-24 03:48:54 +0000

    Initial checkin

--- src/niagara/optimizer/colombia/Attribute.java
+++ src/niagara/optimizer/colombia/Attribute.java
@@ -1,9 +1,11 @@
-/* $Id: Attribute.java,v 1.1 2002/10/24 01:38:06 vpapad Exp $ */
+/* $Id: Attribute.java,v 1.2 2002/10/24 03:48:54 vpapad Exp $ */
 package niagara.optimizer.colombia;
 
 /** Tuple attributes */
 public interface Attribute {
     String getName();
+    
     Domain getDomain();
+    
     Attribute copy();
 } 
