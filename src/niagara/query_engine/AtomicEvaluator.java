8e470ffd4fa20b3018f7d38d3aeb8f49e7c0bac6
vpapad <vpapad>
2002-10-27 02:43:50 +0000

    New atomic evaluator

--- src/niagara/query_engine/AtomicEvaluator.java
+++ src/niagara/query_engine/AtomicEvaluator.java
@@ -1,94 +1,116 @@
+/* $Id: AtomicEvaluator.java,v 1.6 2002/10/27 02:43:50 vpapad Exp $ */
 package niagara.query_engine;
 
 import java.util.ArrayList;
-import org.w3c.dom.*;
+import java.util.HashMap;
 
-import niagara.utils.*;
-import niagara.xmlql_parser.syntax_tree.*;
+import org.w3c.dom.Node;
+
+import niagara.utils.NodeVector;
+import niagara.utils.StreamTupleElement;
+import niagara.xmlql_parser.syntax_tree.regExp;
+import niagara.xmlql_parser.syntax_tree.schemaAttribute;
 
 public class AtomicEvaluator {
-    boolean isConstant;
-    String constant;
-
+    private String name;
+    private regExp path;
+    
     int streamId;
     int attributeId;
+
     PathExprEvaluator pev;
 
-    StreamTupleElement[] oneTuple;
     NodeVector reachableNodes;
 
-    public AtomicEvaluator(Object value) {
-        if (value instanceof String) {
-            isConstant = true;
-            constant = (String) value;
-        } else {
-            isConstant = false;
-            schemaAttribute sa = (schemaAttribute) value;
-	    
-            streamId = sa.getStreamId();
-	    attributeId = sa.getAttrId();
-            pev = new PathExprEvaluator(sa.getPath());
-        }
-        
-        oneTuple = new StreamTupleElement[1];
-        reachableNodes = new NodeVector();
+    /** Initialize an evaluator with a variable's name,
+     *  which later on must be resolved with resolveVariables */
+    public AtomicEvaluator(String name) {
+        this.name = name;
+    }
+
+    public AtomicEvaluator(String name, regExp path) {
+        this.name = name;
+        // XXX vpapad: we never really use path, it is an opportunity
+        // for optimization since it avoids an extra unnest
+        // alternatively using PathExprEvaluator just to get the
+        // tuple attribute itself is overkill
+        this.path = path;
     }
     
+    /** Initialize an evaluator for a specific input attribute */
+    public AtomicEvaluator(schemaAttribute sa) {
+        resolveVariables(sa);
+    }
+
+    public void resolveVariables(TupleSchema ts, int streamId) {
+        if (!ts.contains(name)) return;
+        this.streamId = streamId;
+        this.attributeId = ts.getPosition(name);
+        pev = new PathExprEvaluator(path);
+        reachableNodes = new NodeVector();
+    }
+
+    public void resolveVariables(schemaAttribute sa) {
+        streamId = sa.getStreamId();
+        attributeId = sa.getAttrId();
+        pev = new PathExprEvaluator(sa.getPath());
+        reachableNodes = new NodeVector();
+    }
+
     /**
-     * This function finds the atomic values associated with a given value
-     * in a predicate.
+     * This function appends the atomic values associated with a given value
+     * in a predicate to an ArrayList
      *
-     * @param tuples The tuples over which the value is defined
+     * @param t1    Left incoming tuple
+     * @param t2    Right incoming tuple
      * @param values An ArrayList to add the atomic values in
      *
      * KT - looks to me like atomic values are always strings
      * XXX vpapad: They are!
      */
 
-    void getAtomicValues (StreamTupleElement[] tuples, ArrayList values) {
-	// If the value is of type string, then just add it to result
-	//
-	if (isConstant) 
-	    values.add(constant);
-	else {
-	    // Invoke the path expression evaluator to get the nodes reachable
-	    // using the path expression
-            pev.getMatches(tuples[streamId].
-			   getAttribute(attributeId),
-                           reachableNodes);
+    public void getAtomicValues(
+        StreamTupleElement t1,
+        StreamTupleElement t2,
+        ArrayList values) {
+        StreamTupleElement tuple;
+        
+        if (streamId == 0)
+            tuple = t1;
+        else
+            tuple = t2;
 
-	    // For each reachable node, get the atomic value associated with
-	    // it
-	    int numReachableNodes = reachableNodes.size();
+        // Invoke the path expression evaluator to get the nodes reachable
+        // using the path expression
+        pev.getMatches(tuple.getAttribute(attributeId), reachableNodes);
 
-	    for (int nodeid = 0; nodeid < numReachableNodes; ++nodeid) {
+        // For each reachable node, get the atomic value associated with it
+        int numReachableNodes = reachableNodes.size();
 
-		// First get the node
-		//
-		Node node = reachableNodes.get(nodeid);
+        for (int nodeid = 0; nodeid < numReachableNodes; ++nodeid) {
+            // First get the node
+            Node node = reachableNodes.get(nodeid);
 
-		if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
-		    values.add(node.getNodeValue());
-		    continue;
-		}
+            if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
+                values.add(node.getNodeValue());
+                continue;
+            }
 
-		// Now get its first child
-		//
-		Node firstChild = node.getFirstChild();
+            // Now get its first child
+            Node firstChild = node.getFirstChild();
 
-		// If such a child exists, then add its value to the result
-		//
-		if (firstChild != null) {
-		    
-		    values.add(firstChild.getNodeValue());
-		}
-	    }
-            reachableNodes.clear();
+            // If such a child exists, then add its value to the result
+            if (firstChild != null) {
+                values.add(firstChild.getNodeValue());
+            }
         }
+        reachableNodes.clear();
     }
     
-    void getAtomicValues(StreamTupleElement tuple, ArrayList values) {
-        oneTuple[0] = tuple;
-        getAtomicValues(oneTuple, values);
+    // XXX vpapad: for compatibility with CVS version
+    public void getAtomicValues(
+        StreamTupleElement t1,
+        ArrayList values) {
+        getAtomicValues(t1, null, values);               
     }
 }
