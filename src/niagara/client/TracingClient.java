996014c6b8ece0e51b7c87f2d680d3aa62dab1c2
tufte <tufte>
2003-07-27 20:59:45 +0000

    test

--- src/niagara/client/TracingClient.java
+++ src/niagara/client/TracingClient.java
@@ -1,4 +1,5 @@
 package niagara.client;
+// hello
 public class TracingClient extends SimpleClient {
     boolean firstTime;
     public TracingClient(String host, int port, String outputFileName) {
