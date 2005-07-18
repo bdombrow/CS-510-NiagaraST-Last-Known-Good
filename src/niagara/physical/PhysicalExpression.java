341bd94361bc04866ceab038eec2777acffa6278
acd <acd>
2005-07-18 22:22:20 +0000

    Changed expression to create elements instead of text nodes.

--- src/niagara/physical/PhysicalExpression.java
+++ src/niagara/physical/PhysicalExpression.java
@@ -1,28 +1,28 @@
 /**********************************************************************
-  $Id: PhysicalExpression.java,v 1.2 2003/12/24 02:54:13 vpapad Exp $
+ $Id: PhysicalExpression.java,v 1.3 2005/07/18 22:22:20 acd Exp $
 
 
-  NIAGARA -- Net Data Management System                                 
-                                                                        
-  Copyright (c)    Computer Sciences Department, University of          
-                       Wisconsin -- Madison                             
-  All Rights Reserved.                                                  
-                                                                        
-  Permission to use, copy, modify and distribute this software and      
-  its documentation is hereby granted, provided that both the           
-  copyright notice and this permission notice appear in all copies      
-  of the software, derivative works or modified versions, and any       
-  portions thereof, and that both notices appear in supporting          
-  documentation.                                                        
-                                                                        
-  THE AUTHORS AND THE COMPUTER SCIENCES DEPARTMENT OF THE UNIVERSITY    
-  OF WISCONSIN - MADISON ALLOW FREE USE OF THIS SOFTWARE IN ITS "        
-  AS IS" CONDITION, AND THEY DISCLAIM ANY LIABILITY OF ANY KIND         
-  FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.   
-                                                                        
-  This software was developed with support by DARPA through             
-   Rome Research Laboratory Contract No. F30602-97-2-0247.  
-**********************************************************************/
+ NIAGARA -- Net Data Management System                                 
+ 
+ Copyright (c)    Computer Sciences Department, University of          
+ Wisconsin -- Madison                             
+ All Rights Reserved.                                                  
+ 
+ Permission to use, copy, modify and distribute this software and      
+ its documentation is hereby granted, provided that both the           
+ copyright notice and this permission notice appear in all copies      
+ of the software, derivative works or modified versions, and any       
+ portions thereof, and that both notices appear in supporting          
+ documentation.                                                        
+ 
+ THE AUTHORS AND THE COMPUTER SCIENCES DEPARTMENT OF THE UNIVERSITY    
+ OF WISCONSIN - MADISON ALLOW FREE USE OF THIS SOFTWARE IN ITS "        
+ AS IS" CONDITION, AND THEY DISCLAIM ANY LIABILITY OF ANY KIND         
+ FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.   
+ 
+ This software was developed with support by DARPA through             
+ Rome Research Laboratory Contract No. F30602-97-2-0247.  
+ **********************************************************************/
 
 package niagara.physical;
 
@@ -30,6 +30,7 @@ import niagara.logical.*;
 import niagara.optimizer.colombia.*;
 
 import org.w3c.dom.*;
+
 import java.io.StringReader;
 import niagara.utils.*;
 
@@ -37,32 +38,34 @@ import koala.dynamicjava.interpreter.*;
 import koala.dynamicjava.parser.wrapper.*;
 
 /**
- * The <code>PhysicalExpressionOperator</code> class is derived from the abstract class
- * <code>PhysicalOperator</code>. It implements evaluating an arbitrary Expression 
- * on an incoming tuple, producing a new wider outgoing tuple.
+ * The <code>PhysicalExpressionOperator</code> class is derived from the
+ * abstract class <code>PhysicalOperator</code>. It implements evaluating an
+ * arbitrary Expression on an incoming tuple, producing a new wider outgoing
+ * tuple.
  */
 
 public class PhysicalExpression extends PhysicalOperator {
-    // No blocking input streams
-    private static final boolean[] blockingSourceStreams = { false };
+	// No blocking input streams
+	private static final boolean[] blockingSourceStreams = { false };
 
-    private Expression expressionOp;
+	private Expression expressionOp;
 
-    /** An object of a class that implements ExpressionIF */
-    protected ExpressionIF expressionObject;
-    /** A string to be interpreted for every tuple */
-    private String expression;
+	/** An object of a class that implements ExpressionIF */
+	protected ExpressionIF expressionObject;
 
-    public PhysicalExpression() {
-        setBlockingSourceStreams(blockingSourceStreams);
-    }
+	/** A string to be interpreted for every tuple */
+	private String expression;
 
-    public void opInitFrom(LogicalOp logicalOperator) {
-        // Type cast the logical operator to a Expression operator
-        expressionOp = (Expression) logicalOperator;
-    }
+	public PhysicalExpression() {
+		setBlockingSourceStreams(blockingSourceStreams);
+	}
 
-    public void opInitialize() 
+	public void opInitFrom(LogicalOp logicalOperator) {
+		// Type cast the logical operator to a Expression operator
+		expressionOp = (Expression) logicalOperator;
+	}
+
+public void opInitialize() 
     throws ShutdownException {
         boolean interpreted = expressionOp.isInterpreted();
 
@@ -89,7 +92,9 @@ public class PhysicalExpression extends PhysicalOperator {
                 }
                 source += "int result; " + expression;
                 source
-                    += "; return doc.createTextNode(String.valueOf(result));\n"
+                    += "; Element newNode =  doc.createElement(\"expr\"); \n" 
+                    +  "newNode.appendChild(doc.createTextNode(String.valueOf(result)));\n"
+                    +  "return newNode; \n"
                     + "}} new UserExpression();";
 
                 Interpreter interpreter =
@@ -122,69 +127,66 @@ public class PhysicalExpression extends PhysicalOperator {
                         + iae.getMessage());
             }
         }
-    }
+    }	/**
+		 * This function processes a tuple element read from a source stream
+		 * when the operator is non-blocking. This over-rides the corresponding
+		 * function in the base class.
+		 * 
+		 * @param inputTuple
+		 *            The tuple element read from a source stream
+		 * @param streamId
+		 *            The source stream from which the tuple was read
+		 * 
+		 * @exception ShutdownException
+		 *                query shutdown by user or execution error
+		 */
 
-    /**
-     * This function processes a tuple element read from a source stream
-     * when the operator is non-blocking. This over-rides the corresponding
-     * function in the base class.
-     *
-     * @param inputTuple The tuple element read from a source stream
-     * @param streamId The source stream from which the tuple was read
-     *
-     * @exception ShutdownException query shutdown by user or execution error
-     */
+	protected void processTuple(Tuple inputTuple, int streamId)
+			throws ShutdownException, InterruptedException {
+		Node exprElt = expressionObject.processTuple(inputTuple);
+		if (exprElt != null) {
+			int outSize = outputTupleSchema.getLength();
+			Tuple outputTuple = inputTuple.copy(outSize);
+			outputTuple.setAttribute(outSize - 1, exprElt);
+			// Add the output tuple to the result
+			putTuple(outputTuple, 0);
+		}
+	}
 
-    protected void processTuple(
-        Tuple inputTuple,
-        int streamId)
-        throws ShutdownException, InterruptedException {
-        Node res = expressionObject.processTuple(inputTuple);
-        if(res != null) {
-        	Tuple outputTuple =
-            	(Tuple) inputTuple.clone();
-        	outputTuple.appendAttribute(res);
-        	// Add the output tuple to the result
-        	putTuple(outputTuple, 0);
-        }
-    }
+	public boolean isStateful() {
+		return false;
+	}
 
-    public boolean isStateful() {
-        return false;
-    }
+	/**
+	 * @see niagara.optimizer.colombia.Op#copy()
+	 */
+	public Op opCopy() {
+		PhysicalExpression op = new PhysicalExpression();
+		op.expressionOp = expressionOp;
+		return op;
+	}
 
-    /**
-     * @see niagara.optimizer.colombia.Op#copy()
-     */
-    public Op opCopy() {
-        PhysicalExpression op = new PhysicalExpression();
-        op.expressionOp = expressionOp;
-        return op;
-    }
+	public boolean equals(Object o) {
+		if (o == null || !(o instanceof PhysicalExpression))
+			return false;
+		if (o.getClass() != PhysicalExpression.class)
+			return o.equals(this);
+		PhysicalExpression other = (PhysicalExpression) o;
+		return expressionOp == other.expressionOp;
+	}
 
-    public boolean equals(Object o) {
-        if (o == null || !(o instanceof PhysicalExpression))
-            return false;
-        if (o.getClass() != PhysicalExpression.class)
-            return o.equals(this);
-        PhysicalExpression other = (PhysicalExpression) o;
-        return expressionOp == other.expressionOp;
-    }
+	public int hashCode() {
+		return expressionOp.hashCode();
+	}
 
-    public int hashCode() {
-        return expressionOp.hashCode();
-    }
-
-    /**
-     * @see niagara.optimizer.colombia.PhysicalOp#findLocalCost(ICatalog, LogicalProperty[])
-     */
-    public Cost findLocalCost(
-        ICatalog catalog,
-        LogicalProperty[] inputLogProp) {
-        return new Cost(
-            inputLogProp[0].getCardinality()
-                * (catalog.getDouble("tuple_reading_cost")
-                    + catalog.getDouble("expression_cost")
-                    + catalog.getDouble("tuple_construction_cost")));
-    }
+	/**
+	 * @see niagara.optimizer.colombia.PhysicalOp#findLocalCost(ICatalog,
+	 *      LogicalProperty[])
+	 */
+	public Cost findLocalCost(ICatalog catalog, LogicalProperty[] inputLogProp) {
+		return new Cost(inputLogProp[0].getCardinality()
+				* (catalog.getDouble("tuple_reading_cost")
+						+ catalog.getDouble("expression_cost") + catalog
+						.getDouble("tuple_construction_cost")));
+	}
 }
