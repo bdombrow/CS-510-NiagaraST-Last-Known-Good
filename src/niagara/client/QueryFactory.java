7248455956c95f85b3ff32346c3e486071b663ae
tufte <tufte>
2005-10-09 05:33:36 +0000

    Better yet. Just throw an error on any query that isn't in xml. Not like anyone is going to write XMLQL these days!

--- src/niagara/client/QueryFactory.java
+++ src/niagara/client/QueryFactory.java
@@ -5,10 +5,8 @@ public class QueryFactory {
     public static Query makeQuery(String text) throws ClientException {
 	   if (text.startsWith("<?xml")) 
 	       return new QPQuery(text);
-	   else if (text.toUpperCase().indexOf("WHERE") != -1) 
-	       return new XMLQLQuery(text);
-	   else {
-	       throw new ClientException("Invalid query: " + text);
-		 }
+		 else
+				 throw new ClientException("Invalid Query: " + text);
+	    //   return new XMLQLQuery(text);
     }
 }
