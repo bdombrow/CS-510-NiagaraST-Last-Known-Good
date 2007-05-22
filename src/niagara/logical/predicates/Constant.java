62bc3f35f322913aad2303a7776d6c5f2b3f1db3
vpapad <vpapad>
2007-05-22 01:26:49 +0000

    Fixed equals()

--- src/niagara/logical/predicates/Constant.java
+++ src/niagara/logical/predicates/Constant.java
@@ -1,13 +1,13 @@
-/* $Id */
+/* $Id: Constant.java,v 1.3 2007/05/22 01:26:49 vpapad Exp $ */
 package niagara.logical.predicates;
 
 public abstract class Constant implements Atom {
     public abstract String getValue();
     
     public boolean equals(Object other) {
-        if (other.toString().equals(getValue()))
-            return true;
-        return false;
+       if (other == null || !(other instanceof Constant))
+            return false;
+	return ((Constant) other).getValue().equals(getValue());
     }
     
     public boolean isConstant() { return true; }
