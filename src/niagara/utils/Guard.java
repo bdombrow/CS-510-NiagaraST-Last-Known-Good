8eec06169b7307bdd296f91eb0efb98412b4a4fa
Rafael Fernandez <rfernand@cs.pdx.edu>
2010-02-19 17:48:59 -0800

    Updating Guard after discussion (hash table now holds a oplymorphic object).

--- src/niagara/utils/Guard.java
+++ src/niagara/utils/Guard.java
@@ -16,14 +16,53 @@ import niagara.query_engine.TupleSchema;
  */
 public class Guard {
 	// Fields
-	@SuppressWarnings("unchecked")
-	private Hashtable _guard;
+	private Hashtable<String, GuardObject> _guard;
+
+	private class GuardObject {
+
+		private HashType _type;
+		private Object _value;
+		private Boolean _status;
+
+		public GuardObject(HashType t, Boolean s) throws GuardException{
+			switch(t) {
+			case INTEGER:
+				_type = t;
+				_value = 0;
+				_status = s;
+			default:
+				throw new GuardException("Unsupported type in Guard Object");
+			}
+		}
+
+		public int getValue() throws GuardException {
+			if(this._type == HashType.INTEGER)
+				return (Integer)_value;
+			else throw new GuardException("Attempted to get integer from non-integer Guard Object");
+		}
+
+		public Boolean getStatus(){
+			return _status;
+		}
+
+
+		public void updateValue(int o) throws GuardException{
+			if(this._type == HashType.INTEGER)
+				_value = o;
+			else
+				throw new GuardException("Attempted to update a non-integer Guard Object.");
+		}
+
+		public void updateStatus(Boolean s) {
+			_status = s;	
+		}
+	}
 
 	// Properties
 	/**
 	 * Supported hash types
 	 */
-	public enum HashType { BOOLEAN, INTEGER };
+	public enum HashType { INTEGER };
 
 	// Ctor
 	/**
@@ -31,15 +70,8 @@ public class Guard {
 	 * 
 	 * @param t type of hash value
 	 */
-	public Guard(HashType t) throws GuardException{
-		switch(t) {
-		case BOOLEAN :
-			_guard = new Hashtable<String,Boolean>();
-		case INTEGER:
-			_guard = new Hashtable<String,Integer>();
-		default :
-			throw new GuardException("Unsupported Guard type");
-		}
+	public Guard(){
+		_guard = new Hashtable<String,GuardObject>();
 	}
 
 	// Methods
@@ -50,12 +82,12 @@ public class Guard {
 	 * 
 	 * @return hash key (string) for use in guards
 	 */
-	public static String hashKey(Vector groupAttributeList, TupleSchema schema, Tuple tuple) {
+	public static String hashKey(Vector<Variable> groupAttributeList, TupleSchema schema, Tuple tuple) {
 		Variable groupAttribute;
 		int position;
 		String key = "";		
 		for( int i=0; i < groupAttributeList.size(); i++ ) {
-			groupAttribute = (Variable)groupAttributeList.elementAt(i);
+			groupAttribute = groupAttributeList.elementAt(i);
 			position = schema.getPosition(groupAttribute.getName());
 			key.concat(((BaseAttr)tuple.getAttribute(position)).attrVal().toString());		
 		}
@@ -65,10 +97,17 @@ public class Guard {
 	/**
 	 * 
 	 * @param key hash key
-	 * @param o hash entry's value
+	 * @param i hash entry's value
+	 * @param s status of guard
 	 */
-	public void add(String key, Object o) {
-		_guard.put(key, o);
+	public void add(String key, Integer i, Boolean s) {
+		try {
+			GuardObject o = new GuardObject(HashType.INTEGER, s);
+			o.updateValue(i);
+			_guard.put(key, o);
+		} catch (GuardException o) {
+			System.err.println(o.getMessage());
+		}
 	}
 
 	/** Check if given key is in the hash table
@@ -85,13 +124,39 @@ public class Guard {
 	/**
 	 * 
 	 * @param key hash key
-	 * @return object in hash table
+	 * @return integer value in hash table
 	 * @throws Exception
 	 */
-	public Object getValue(String key) throws GuardException{
+	public int getValue(String key) throws GuardException{
 		if (inHash(key)) {
-			return _guard.get(key);
+			return _guard.get(key).getValue();
 		}
 		else throw new GuardException("Can't return value for non-existing key");
+	}
+
+	/**
+	 * 
+	 * @param key hash key
+	 * @return guard status
+	 * @throws GuardException
+	 */
+	public Boolean getStatus(String key) throws GuardException {
+		if (inHash(key)) {
+			return _guard.get(key).getStatus();
+		}
+		else throw new GuardException("Can't return status for non-existing key");
+
+	}
+
+	/**
+	 * 
+	 * @param key hash key
+	 * @param s status
+	 * @throws GuardException
+	 */
+	public void updateStatus(String key, Boolean s) throws GuardException {
+		if(inHash(key)) {
+			_guard.get(key).updateStatus(s);
+		}
 	}
 }
